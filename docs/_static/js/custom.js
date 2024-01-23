6e96b960b08ba72492e97e1612a382494e568563
Valentin Niess <niess@in2p3.fr>
2024-01-23 11:54:53 +0100

    Link to Nord colour palette

--- docs/_static/js/custom.js
+++ docs/_static/js/custom.js
@@ -1,10 +1,21 @@
-/* Add external link to commit hash. */
 jQuery(document).ready(function(){
-        var element = jQuery("span.commit").find("code").first();
+        /* Add external link to commit hash. */
+        var element = jQuery("li.wy-breadcrumbs-aside>a.fa.fa-github");
         if (element.length) {
-                const commit = element.text();
-                element.replaceWith(
-                    `<a href="https://github.com/niess/goupil/tree/${commit}">${commit}<a>`
-                );
+                const base_url = element[0]
+                        .href
+                        .split("/blob/")[0];
+                var element = jQuery("footer span.commit>code").first();
+                if (element.length) {
+                        const commit = element.text();
+                        element.replaceWith(
+                                `<a href="${base_url}/tree/${commit}">${commit}<a>`
+                        );
+                }
         }
+
+        /* Add reference to Nord colour palette. */
+        var element = jQuery("footer>a:contains('Read the Docs')")
+                .first()
+                .after(" and <a href='https://www.nordtheme.com/'>Nord</a> colours");
 });
